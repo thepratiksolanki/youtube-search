ccbae6ef98b0148a9730b9061c498c82aaa35880
Pratik Solanki <pratiks.solanki3@gmail.com>
2020-01-31 20:11:24 +0530

    Configuring the youtube external library and setting up the credentials for the API call

--- config/app.php
+++ config/app.php
@@ -175,6 +175,12 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Youtube API Providers...
+         */
+        
+        Alaouy\Youtube\YoutubeServiceProvider::class,
+
     ],
 
     /*
@@ -225,7 +231,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Youtube' => Alaouy\Youtube\Facades\Youtube::class,
     ],
 
 ];
